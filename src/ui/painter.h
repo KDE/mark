157a184f2f7c23b9d80b015e9d250f57480e515f
Jean Lima Andrade <jeno.andrade@gmail.com>
2020-07-02 12:21:02 -0300

    Initial documentation of Painter

--- src/ui/painter.h
+++ src/ui/painter.h
@@ -7,19 +7,40 @@
 
 class QGraphicsItem;
 
+/** Base class, responsible for load, display and annotate data. */
 class Painter
 {
 public:
+    /** Constructor of Painter.
+     * @param parent - Container instance to use.
+     */
     explicit Painter(Container* parent);
     ~Painter();
 
+    /** @return pointer to the current instance of QGraphicsItem. */
     QGraphicsItem* currentItem() const { return m_currentItem; }
 
+    /** paint given point.
+     * @param point - point to paint.
+     */
     virtual void paint(QPoint point) = 0;
+
+    /** Display given object.
+     * @param object - MarkedObject instance to display.
+     */
     virtual void paintObject(MarkedObject* object) = 0;
+
+    /** Load the current MarkedObjects. */
     virtual void repaint() = 0;
+
+    /** Load given item base on its path.
+     * @param path - item path.
+     */
     virtual void changeItem(const QString& path) = 0;
 
+    /** Import Objects to the parent.
+     * @param objects - objects to load.
+     */
     virtual bool importObjects(QVector<MarkedObject*> objects) = 0;
 
 protected:
